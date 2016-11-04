0304ef712f7d67ce4e05762c18f4806435a164ed
aeoleader <aeoleader@gmail.com>
2016-11-04 11:03:26 +0800

    Draw CGDT

--- 2DDrawingTemplate/main.cpp
+++ 2DDrawingTemplate/main.cpp
@@ -8,6 +8,9 @@
 // Std. Includes
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
 //GLFW
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -25,9 +28,26 @@
 #include <glm/gtc/type_ptr.hpp>
 
 //Other Libs
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_conformer_2.h>
 
+#include <CGAL/Random.h>
+#include <CGAL/point_generators_2.h>
+#include <CGAL/Timer.h>
 //Typedefs
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point_2;
+typedef K::Iso_rectangle_2 Iso_rectangle_2;
+typedef K::Segment_2 Segment_2;
+typedef K::Ray_2 Ray_2;
+typedef K::Line_2 Line_2;
+typedef CGAL::Delaunay_triangulation_2<K>  Delaunay_triangulation_2;
 
+typedef CGAL::Constrained_Delaunay_triangulation_2<K> CDT;
+typedef CDT::Point Point;
+typedef CDT::Vertex_handle Vertex_handle;
 //Properties
 GLuint screenWidth = 800, screenHeight = 600;
 
@@ -38,7 +58,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void Do_Movement();
 
 //Other function prototypes
-
+void generateData(GLfloat** vertices);
+Delaunay_triangulation_2 delaunayTriangulation(std::vector<Point_2> pts);
+CDT conformingGabrielDT(std::vector<Point> pts);
 //Camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 bool keys[1024];
@@ -49,6 +71,8 @@ bool firstMouse = true;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
+GLuint faceCount = 0;
+GLuint vertsCount = 0;
 //Namespaces
 
 int main(int argc, const char * argv[]) {
@@ -86,12 +110,15 @@ int main(int argc, const char * argv[]) {
     Shader ourShader("VertexShader.vert", "FragmentShader.frag");
     
     //Create vertices
-    GLfloat vertices[] = {
-        0.0f, 0.5f, 0.0f,
-        -0.5f, 0.0f, 0.0f,
-        0.5f, 0.0f, 0.0f
-    };
-    
+//    GLfloat vertices[] = {
+//        0.0f, 0.5f, 0.0f,
+//        -0.5f, 0.0f, 0.0f,
+//        0.5f, 0.0f, 0.0f
+//    };
+    GLfloat* vertices;
+    generateData(&vertices);
+//    for (int i = 0; i < 3 * vertsCount; i+=3)
+//        std::cout << vertices[i] << " " << vertices[i+1] << " " << vertices[i + 2] << std::endl;
     //Setup VBO and VAO
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -100,7 +127,7 @@ int main(int argc, const char * argv[]) {
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3*vertsCount*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
     
     //Position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
@@ -148,7 +175,8 @@ int main(int argc, const char * argv[]) {
         //model = glm::rotate(model, angle, glm::vec3(0.0f, 0.0f, 1.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         //Draw trangles
-        glDrawArrays(GL_LINES, 0, 4);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawArrays(GL_TRIANGLES, 0, vertsCount);
         glBindVertexArray(0);
         glfwSwapBuffers(window);
     }
@@ -211,3 +239,82 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
     camera.ProcessMouseScroll(yoffset);
 }
 
+void generateData(GLfloat** vertices)
+{
+    typedef CGAL::Creator_uniform_2<double, Point_2> Creator;
+    CGAL::Random random(7);
+    CGAL::Random_points_in_square_2<Point_2, Creator> in_square(.5, random);
+    int n = 100;
+    std::vector<Point_2> pts;
+    std::vector<Point> CGDT_pts;
+    for (int i = 0 ; i < n ; i++)
+    {
+        Point_2 p = *in_square;
+        in_square++;
+        pts.push_back(Point_2(p.x() + .5, p.y() + .5));
+        CGDT_pts.push_back(Point(p.x() + .5, p.y() + .5));
+        //std::cout << pts[i].x() << " " << pts[i].y() << std::endl;
+    }
+    
+    //Delaunay_triangulation_2 dt2 = delaunayTriangulation(pts);
+    //Delaunay_triangulation_2::Finite_faces_iterator it;
+    
+    CDT dt2 = conformingGabrielDT(CGDT_pts);
+    CDT::Finite_faces_iterator it;
+    faceCount = dt2.number_of_faces();
+    vertsCount = 3 * faceCount;
+    *vertices = new GLfloat[9 * faceCount];
+    std::cout << "Number of Faces: " << faceCount << std::endl;
+    int index = 0;
+    
+    for (it = dt2.finite_faces_begin(); it != dt2.finite_faces_end(); it++)
+    {
+        
+        (*vertices)[index    ] = dt2.triangle(it).vertex(0).x();
+        (*vertices)[index + 1] = dt2.triangle(it).vertex(0).y();
+        (*vertices)[index + 2] = 0.0f;
+        
+        index += 3;
+        (*vertices)[index    ] = dt2.triangle(it).vertex(1).x();
+        (*vertices)[index + 1] = dt2.triangle(it).vertex(1).y();
+        (*vertices)[index + 2] = 0.0f;
+        
+        index += 3;
+        (*vertices)[index    ] = dt2.triangle(it).vertex(2).x();
+        (*vertices)[index + 1] = dt2.triangle(it).vertex(2).y();
+        (*vertices)[index + 2] = 0.0f;
+        
+        index += 3;
+    }
+    
+    std::ofstream outfile("output.out");
+    if (!outfile.is_open()) return;
+    for (int i = 0; i < 3 * vertsCount; i+=3)
+    {
+        outfile << (*vertices)[i] << " " <<(*vertices)[i + 1] << " " << (*vertices)[i + 2]<< std::endl;
+    }
+
+}
+
+Delaunay_triangulation_2 delaunayTriangulation(std::vector<Point_2> pts)
+{
+   
+    Delaunay_triangulation_2 dt2;
+    dt2.insert(pts.begin(), pts.end());
+    return dt2;
+}
+
+CDT conformingGabrielDT(std::vector<Point> pts)
+{
+    CDT cdt;
+    for (int i = 0; i < pts.size(); i ++)
+    {
+        //insert points
+        cdt.insert(pts[i]);
+    }
+    CGAL::make_conforming_Delaunay_2(cdt);
+    
+    CGAL::make_conforming_Gabriel_2(cdt);
+    return cdt;
+    
+}
